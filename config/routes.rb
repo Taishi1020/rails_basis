7a010be54c62bf9150d1f1a6f7a10a17f5823f85
Taishi1020 <tai.yakiman1020@gmail.com>
2021-10-23 15:26:28 +0900

    ウェブページに表示
    1routingの設定
    2controllerの設定
    3viewの設定（文字出力）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #root localhost:3000番にアクセスが来たら BoardsControllerのindexにアクセスする意味
+  root 'boards#index'
 end
